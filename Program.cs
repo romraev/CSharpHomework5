3e3e0577bfa783211d5b9d4c32c67f74ab836836
Romraev <romraev@yandex.ru>
2023-03-23 20:16:41 +0300

    third task

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ Console.WriteLine($"{Skolko(newArray)} чётных чисел.");
 // Task 36. Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
 
-int[] CreateRandomArray(int size, int min, int max)
+/* int[] CreateRandomArray(int size, int min, int max)
 {
     int[] array = new int[size];
     
@@ -86,3 +86,60 @@ Console.WriteLine("В массиве: ");
 WriteArray(newArray);
 Console.WriteLine();
 Console.WriteLine($"Сумма нечетных по индексу чисел равна {Sum(newArray)}.");
+*/
+
+// Task 38. Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] CreateRandomArray(int size, int min,  int max)
+{
+    double[] array = new double[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max) + new Random().NextDouble();
+    }
+    return array;
+}
+
+void WriteArray(double[]array)
+{
+    for(int i=0; i<array.Length; i++)
+        Console.WriteLine(array[i]);
+}
+
+double Max(double[] array)
+{
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i]>max) max = array[i];
+    }
+    return max;
+}
+
+double Min(double[] array)
+{
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i]<min) min = array[i];
+    }
+    return min;
+}
+
+Console.Write("Введите необходимое количество чисел в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение: ");
+int mi = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение: ");
+int ma = Convert.ToInt32(Console.ReadLine());
+
+double[] newArray = CreateRandomArray(N, mi, ma);
+
+Console.WriteLine("В массиве: ");
+WriteArray(newArray);
+Console.WriteLine();
+Console.WriteLine($"Разница минимального и максимального чисел равна {Max(newArray) - Min(newArray)}");
